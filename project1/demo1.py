2e2a6acad4a4ed5fc00d19d64d18abc73a184eef
gbs <2591787948@qq.com>
2021-01-22 09:48:31 +0800

    新建了demo1

---
+++ project1/demo1.py
@@ -0,0 +1,13 @@
+"""
+author:gbs
+c_date:2021/1/22 9:38
+u_date:2021/1/22 9:38
+reversion:1.0
+file_name:demo1
+"""
+from flask import Flask
+
+app = Flask
+
+if __name__ == "__main__":
+    app.run()

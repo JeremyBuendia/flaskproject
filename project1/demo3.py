85860caaf1c736c7cc3d5b6045d9e27e087a1e51
gbs <2591787948@qq.com>
2021-01-22 10:38:54 +0800

    新增了flask的模板

---
+++ project1/demo3.py
@@ -0,0 +1,20 @@
+"""
+author:gbs
+c_date:2021/1/22 10:31
+u_date:2021/1/22 10:31
+reversion:1.0
+file_name:demo3
+"""
+from flask import Flask,render_template
+
+app = Flask(__name__)
+
+@app.route("/")
+def index():
+    name="陈乐"
+    # sex ="女"
+    return render_template("index.html",**locals())
+
+if __name__ == '__main__':
+    app.run(debug=True)
+
